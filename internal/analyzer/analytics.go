85182bb864162369173f0bd05bc167a47b4ebfcc
Hundera Awoke <hunderaweke@gmail.com>
2025-12-22 21:33:05 +0300

    Refactor analytics processing and structures

--- internal/analyzer/analytics.go
+++ internal/analyzer/analytics.go
@@ -6,90 +6,112 @@ import (
 	"github.com/gotd/td/tg"
 )
 
+type OverallMetrics struct {
+	TotalViews     int `json:"total_views"`
+	TotalComments  int `json:"total_comments"`
+	TotalReactions int `json:"total_reactions"`
+	TotalPosts     int `json:"total_posts"`
+	TotalForwards  int `json:"total_forwards"`
+}
+
+func (m *OverallMetrics) UpdateMetrics(msg *tg.Message) {
+	_, totalReactions := countNumOfReactions(msg.Reactions)
+	m.TotalViews += msg.Views
+	m.TotalReactions += totalReactions
+	m.TotalComments += msg.Replies.Replies
+	_, ok := msg.FwdFrom.GetFromID()
+	if !ok {
+		return
+	}
+	m.TotalForwards += 1
+}
+
+type TimeTrends struct {
+	ViewsByMonth         map[string]int   `json:"views_by_month,omitempty"`
+	PostsByDay           map[string][]int `json:"posts_by_day,omitempty"`
+	PostsByMonth         map[string]int   `json:"posts_by_month,omitempty"`
+	PostsByHour          map[int]int      `json:"posts_by_hour,omitempty"`
+	LongestPostingStreak int              `json:"longest_posting_streak,omitempty"`
+}
+
+func (t *TimeTrends) UpdateTrends(mm *tg.Message) {
+	dateTime := getDateTime(mm.Date)
+	t.ViewsByMonth[dateTime.Month().String()] += mm.Views
+	t.PostsByMonth[dateTime.Month().String()] += 1
+	t.PostsByHour[dateTime.Hour()] += 1
+	month := dateTime.Month().String()
+	tt := time.Date(dateTime.Year(), dateTime.Month()+1, 1, 0, 0, 0, 0, time.UTC)
+	lastDay := tt.AddDate(0, 0, -1)
+	if len(t.PostsByDay[month]) == 0 {
+		t.PostsByDay[month] = make([]int, lastDay.Day())
+	}
+	t.PostsByDay[month][dateTime.Day()-1] += 1
+}
+
+type TopPosts struct {
+	MostViewedID       int            `json:"most_viewed_id"`
+	MostViewedCount    int            `json:"most_viewed_count"`
+	MostCommentedID    int            `json:"most_commented_id"`
+	MostCommentedCount int            `json:"most_commented_count"`
+	ForwardsBySource   map[int]int    `json:"forwards_by_post"`
+	ReactionsByType    map[string]int `json:"reactions_by_type"`
+}
+
+func (tp *TopPosts) UpdateTopPosts(msg *tg.Message) {
+	if tp.MostViewedID == 0 || tp.MostViewedCount < msg.Views {
+		tp.MostViewedID = msg.ID
+		tp.MostViewedCount = msg.Views
+	}
+	if tp.MostCommentedID == 0 || tp.MostCommentedCount < msg.Replies.Replies {
+		tp.MostCommentedID = msg.ID
+		tp.MostCommentedCount = msg.Replies.Replies
+	}
+	reactionCounter, _ := (countNumOfReactions(msg.Reactions))
+	tp.ReactionsByType = mergeMaps(tp.ReactionsByType, reactionCounter)
+	fromID, ok := msg.FwdFrom.GetFromID()
+	if !ok {
+		return
+	}
+	if ch, ok := fromID.(*tg.PeerChannel); ok {
+		tp.ForwardsBySource[int(ch.ChannelID)] += 1
+	}
+}
+
 type Analytics struct {
-	ChannelName            string           `json:"channel_name,omitempty"`
-	TotalViews             int              `json:"total_views,omitempty"`
-	TotalComments          int              `json:"total_comments,omitempty"`
-	TotalReactions         int              `json:"total_reactions,omitempty"`
-	TotalPosts             int              `json:"total_posts,omitempty"`
-	TotalForwards          int              `json:"total_forwards,omitempty"`
-	ViewsByMonth           map[string]int   `json:"views_by_month,omitempty"`
-	ReactionsByType        map[string]int   `json:"reactions_by_type,omitempty"`
-	PostsByHour            map[int]int      `json:"posts_by_hour,omitempty"`
-	PostsByDay             map[string][]int `json:"posts_by_day,omitempty"`
-	PostsByMonth           map[string]int   `json:"posts_by_month,omitempty"`
-	MostViewedPostID       int              `json:"most_viewed_post_id,omitempty"`
-	MostViewedPostCount    int              `json:"most_viewed_post_count,omitempty"`
-	MostCommentedPostID    int              `json:"most_commented_post_id,omitempty"`
-	MostCommentedPostCount int              `json:"most_commented_post_count,omitempty"`
-	ForwardsFromCount      map[int]int      `json:"forwards_from_count,omitempty"`
-	LongestPostingStreak   int              `json:"longest_posting_streak,omitempty"`
-	visited                map[int]struct{}
+	ChannelName string         `json:"channel_name"`
+	Totals      OverallMetrics `json:"totals"`
+	Trends      TimeTrends     `json:"trends"`
+	Highlights  TopPosts       `json:"highlights"`
 }
 
 func NewAnalytics(name string) Analytics {
 	var a Analytics
 	a.ChannelName = name
-	a.ViewsByMonth = make(map[string]int)
-	a.ReactionsByType = make(map[string]int)
-	a.PostsByDay = make(map[string][]int)
-	a.PostsByMonth = make(map[string]int)
-	a.ForwardsFromCount = make(map[int]int)
-	a.visited = make(map[int]struct{})
-	a.PostsByHour = make(map[int]int)
+	a.Trends.ViewsByMonth = make(map[string]int)
+	a.Highlights.ReactionsByType = make(map[string]int)
+	a.Trends.PostsByDay = make(map[string][]int)
+	a.Trends.PostsByMonth = make(map[string]int)
+	a.Highlights.ForwardsBySource = make(map[int]int)
+	a.Trends.PostsByHour = make(map[int]int)
 	return a
-}
-func (a *Analytics) addDateCount(date time.Time) {
-	month := date.Month().String()
-	t := time.Date(date.Year(), date.Month()+1, 1, 0, 0, 0, 0, time.UTC)
-	lastDay := t.AddDate(0, 0, -1)
-	if len(a.PostsByDay[month]) == 0 {
-		a.PostsByDay[month] = make([]int, lastDay.Day())
-	}
-	a.PostsByDay[month][date.Day()-1] += 1
 }
 func (a *Analytics) updateFromChannelMessages(m *tg.MessagesChannelMessages) int {
 	offSet := 0
-	last := &tg.Message{}
-	for _, msg := range m.Messages {
-		if mm, ok := msg.(*tg.Message); ok {
-			if _, ok := a.visited[mm.ID]; ok {
-				continue
-			}
-			if a.MostViewedPostID == 0 || a.MostViewedPostCount < mm.Views {
-				a.MostViewedPostID = mm.ID
-				a.MostViewedPostCount = mm.Views
-			}
-			if a.MostCommentedPostID == 0 || a.MostCommentedPostCount < mm.Replies.Replies {
-				a.MostCommentedPostID = mm.ID
-				a.MostCommentedPostCount = mm.Replies.Replies
-			}
-			offSet = mm.Date
-			a.TotalViews += mm.Views
-			a.TotalComments += mm.Replies.Replies
-			t := getDateTime(mm.Date)
-			a.ViewsByMonth[t.Month().String()] += mm.Views
-			reactionCounter, totalReactions := (countNumOfReactions(mm.Reactions))
-			a.ReactionsByType = mergeMaps(a.ReactionsByType, reactionCounter)
-			a.TotalReactions += totalReactions
-			a.PostsByMonth[t.Month().String()] += 1
-			a.PostsByHour[t.Hour()] += 1
-			a.addDateCount(t)
-			if fromID, ok := mm.FwdFrom.GetFromID(); ok {
-				if ch, ok := fromID.(*tg.PeerChannel); ok {
-					a.ForwardsFromCount[int(ch.ChannelID)] += 1
-				}
-				a.TotalForwards += 1
-			}
-			last = mm
+	for i, msg := range m.Messages {
+		mm, ok := msg.(*tg.Message)
+		if !ok || i == 0 {
+			continue
 		}
+		offSet = mm.Date
+		a.Highlights.UpdateTopPosts(mm)
+		a.Totals.UpdateMetrics(mm)
+		a.Trends.UpdateTrends(mm)
 	}
-	a.visited[last.ID] = struct{}{}
 	return offSet
 }
 func (a *Analytics) GetLongestStreak() {
 	array := make([]int, 0)
-	for _, m := range a.PostsByDay {
+	for _, m := range a.Trends.PostsByDay {
 		array = append(array, m...)
 	}
 	current := 0
@@ -105,5 +127,5 @@ func (a *Analytics) GetLongestStreak() {
 		current = max(current, right-left)
 		left = right
 	}
-	a.LongestPostingStreak = current
+	a.Trends.LongestPostingStreak = current
 }
